dba9a11e59f05ac024c9937d8d0049f4a6eb591a
Shell <106093343+Shell0110@users.noreply.github.com>
2024-02-04 05:36:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,12 @@
 let totalClicks = 0;
 let currentIncrement = 1;
 
-function myClick() {  
-    
+function myClick() {    
     totalClicks = totalClicks + currentIncrement;
-
     refresh();
 }
 
-
-
 // Function Add / Shop
-
-// function add10() {
-    
-//     if(totalClicks >= 100) {
-//         currentIncrement += 10;
-//         totalClicks -= 100;
-//     } else {
-//         alert("Vous devez avoir 100 clicks !")
-//     }
-//     refresh();
-// }
-
-
-// function add100() {
-//     if(totalClicks >= 1000) {
-//         currentIncrement += 100;
-//         totalClicks -= 1000;
-//     } else {
-//         alert("Vous devez avoir 1k clicks !")
-//     }
-//     refresh();
-// }
-
-// function add10k() {
-//     if(totalClicks >= 100000) {
-//         currentIncrement += 10000;
-//         totalClicks -= 100000;
-//     } else {
-//         alert("Vous devez avoir 100k clicks !");
-//     }
-    
-//     refresh();
-// }
-
-// function add1m() {
-//     if(totalClicks >= 100000000) {
-//         currentIncrement += 1000000;
-//         totalClicks -= 100000000;
-//     } else {
-//         alert("Vous devez avoir 1M clicks !")
-//     }
-//     refresh();
-// }
-
-
 
 function refresh() {
     document.getElementById("score").innerHTML = totalClicks;
@@ -83,27 +34,9 @@ function int() {
             },2000)
             start = true;
         } 
-    }, 1000 );
-    
-    
+    }, 1000 );    
 }
 
-
-function interval() {
-    return 0;
-    
-}
-
-// function int() {
-//     let lambda = () => {
-//         totalClicks += 1;
-//         refresh();
-//     }
-    
-//     lambda();
-//     let id = setInterval(lambda, 1000);
-//     setTimeout(()=> clearInterval(id), 3000);
-// }
 
 
 
